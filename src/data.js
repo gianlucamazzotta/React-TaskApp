3ddf74fdfff29352699615427d121b6c20f5d7ec
gianlucamazzotta <gianlucamazzotta.t@gmail.com>
2023-04-13 13:18:41 -0300

    se agrego el proyecto

---
+++ src/data.js
@@ -0,0 +1,17 @@
+export const data = [
+  {
+    id: 0,
+    title: "First Task",
+    description: "Mi primera tarea",
+  },
+  {
+    id: 1,
+    title: "Second Task",
+    description: "Mi segunda tarea",
+  },
+  {
+    id: 2,
+    title: "Third Task",
+    description: "Mi tercera tarea",
+  },
+];
